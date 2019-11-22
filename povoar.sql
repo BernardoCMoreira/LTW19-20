fe15b8ce6e8724a55248f13c48165e2ae471e92d
MiguelPedrosa <miguelPedrosa@gmail.com>
2019-11-22 13:03:19 +0000

    Adicionado informaçao para dases de dados

---
+++ povoar.sql
@@ -0,0 +1,17 @@
+INSERT INTO user VALUES (1, "andreMORestivo@gmail.com", "andreRestivo", "1234", "Andre Restivo");
+INSERT INTO user VALUES (2, "josealves@gmail.com", "Jose001", "DrPbs74JYQQd9Jy", "Jose Alves");
+INSERT INTO user VALUES (3, "rita1998@gmail.com", "rita_23", "reileao10", "Ana Rita Santos");
+INSERT INTO user VALUES (4, "sergioNunes@gmail.com", "sergioNunes", "1234", "Sergio Sobral Nunes");
+
+INSERT INTO property VALUES (1, 1, "Rua Dr. Roberto Frias, s/n 4200-465", "Porto", "Portugal", 3, "Casa agradável", 500);
+INSERT INTO property VALUES (2, 3, "Largo de Ramos 1861","Vila Nova de Gaia", "Portugal", 2, "Casa também agradavel", 450);
+
+INSERT INTO rent VALUES (1, 1, 2, "2019-12-20", "2019-12-31", "2019-12-18", 400);
+INSERT INTO rent VALUES (2, 2, 4, "2020-01-01", "2020-01-31", "2019-12-01", 425);
+INSERT INTO rent VALUES (3, 2, 1, "2019-10-01", "2019-10-31", "2019-09-30", 300);
+
+INSERT INTO rating VALUES (1, 4.5, "Realmente bastante agradável");
+INSERT INTO rating VALUES (3, 5, "Realmente bastante agradável");
+
+INSERT INTO image VALUES ( 1, 1, 1, 1);
+INSERT INTO image VALUES ( 2, 2, 3, 1);
