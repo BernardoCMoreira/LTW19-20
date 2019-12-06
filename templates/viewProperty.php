bd6d9968c641c86f96f33b6707964c70fdabb925
Bernardo Costa Moreira <bernardo1998.99@sapo.pt>
2019-12-06 15:00:08 +0000

    viewProperty added

---
+++ templates/viewProperty.php
@@ -0,0 +1,40 @@
+<br> 
+    <div class=property>
+    <article>
+            
+            <h1>Largo de Ramos 18611</h1>
+            <ul class="imgs">
+                                    <li>
+                        <img src="../images/2.jpg" alt="house " width="500" height="300">
+                    </li>
+                            </ul>
+            <div class="dates">
+            <label for="date"> Starting Date</label><br>
+            <input type="date" id="date" name="date" >
+            <br>
+            <br>
+            <label for="date"> End Date</label><br>
+            <input type="date" id="date" name="date" >
+            <br>
+            </div>
+            <div class="information">
+            <h3>Information</h3>
+            <p>Vila Nova de Gaia, Portugal</p>
+            <p>Bedrooms: 2</p>
+            <p>Bathrooms: 1</p>
+            <p>Description: Casa também agradavel</p>
+            <p>Price Per Day: 450.0€</p>
+            </div>
+            <div class="extras">
+            <h3>Extras</h3>
+            <p>Wi-Fi</p>
+            <p>TV</p>
+            <p>Kitchen</p>
+          
+</div>      
+            <div class="totalPrice">
+                <h2>Total: 450*Days €
+            </div>  
+            <input type="submit" value="Rent">
+        </article>
+    </div>
