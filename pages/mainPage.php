4def2ca6233be0ea02817965aba98999b76bea44
MiguelPedrosa <miguelPedrosa@gmail.com>
2019-12-06 15:44:08 +0000

    Modified property display information

--- pages/mainPage.php
+++ pages/mainPage.php
@@ -1,7 +1,20 @@
 <?php
-  include_once('../config/init.php');
-  
-  include ('../templates/commom/header.php');
-  include ('../templates/search_menu.php');
-  include ('../templates/commom/footer.php');
+	include_once('../config/init.php');
+
+	include ('../templates/commom/header.php');
+	echo '<div class="mainpage">';
+	include ('../templates/search_menu.php');
+
+	include_once('../database/property.php');
+	include_once('../database/image.php');
+	$properties = getRandomNumberOfProperties(3);
+
+	echo '<div class="results">';
+	foreach($properties as $property) {
+		displayProperty($property);
+	}
+	echo '</div>';
+	echo '</div>';
+
+	include ('../templates/commom/footer.php');
 ?>
