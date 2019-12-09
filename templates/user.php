37ce08da3a90b05f750111eeb5670d231ff8ce7b
Bernardo Costa Moreira <bernardo1998.99@sapo.pt>
2019-12-09 10:22:56 +0000

    user card done

--- templates/user.php
+++ templates/user.php
@@ -1,30 +1,31 @@
 <br>
 <div class="userprofile">
   <form>
+  <div class="username">
+      <p>@<?=$user['username']?></p>
+    </div>
+  
     <div class="profile_photo">
       <?php 
         $src_img = "../images/" .  $user['photo'];
         echo '<img src=' . $src_img . ' alt="Logo " width="150" height="150" />'; 
       ?>
     </div>
-    
-    <div class="username">
-      <p>Username: <?=$user['username']?></p>
-    </div>
+    <div class="user_info">
 
     <div class="name">
-      <p>Name: <?=$user['name']?></p>
+      <p> <?=$user['name']?></p>
     </div>
-
+    
     <div class="email">
-      <p>Email: <?=$user['email']?></p>
+      <p><?=$user['email']?></p>
     </div>
 
     <div class="description">
-      <p>Description: <?=$user['description']?></p>
+      <p><?=$user['description']?></p>
     </div>
-    
-    <input type="button" value="Edit" onclick="window.location.href='../pages/userEdit.php'" />
+</div>
+    <input type="button" value="Edit Profile" onclick="window.location.href='../pages/userEdit.php'" />
     
     <input type="submit" value="Add New Property">
   </form>
