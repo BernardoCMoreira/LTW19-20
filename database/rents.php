d9659deb481df922fa2ed97f766987be77bd9d2b
MiguelPedrosa <miguelPedrosa@gmail.com>
2019-12-18 00:42:51 +0000

    Added functions for add rent action

--- database/rents.php
+++ database/rents.php
@@ -18,6 +18,58 @@
 		return $stmt->fetchAll();
 	}
 
+	function isPropertyAvalableFromTo($propertyID, $startDate, $endDate) {
+		global $conn;
+		
+		$stmt = $conn->prepare('SELECT COUNT(*) AS count FROM rent 
+			WHERE propertyID = ? AND startDate <= ? AND endDate <= ?');
+		$stmt->execute(array($propertyID, $startDate, $endDate));
+		$result = $stmt->fetch();
+		return $result['count'] == 0;
+	}
+
+	function isUserFreeFromTo($touristID, $startDate, $endDate) {
+		global $conn;
+		
+		$stmt = $conn->prepare('SELECT COUNT(*) AS count FROM rent WHERE touristID = :touristID
+			AND ((startDate <= :endDate AND startDate >= :endDate) OR
+			(endDate >= :startDate AND endDate <= :startDate) OR
+			(startDate <= :startDate AND endDate >= :endDate))');
+		$stmt->bindParam(':touristID', $touristID);
+		$stmt->bindParam(':startDate', $startDate);
+		$stmt->bindParam(':endDate', $endDate);
+		$stmt->execute();
+		$result = $stmt->fetch();
+
+		return $result['count'] == 0;
+	}
+
+	function calculatePrice($propertyID, $startDate, $endDate) {
+		global $conn;
+
+		$days = (strtotime($endDate) - strtotime($startDate)) / (24*60*60);
+		
+		$stmt = $conn->prepare('SELECT price FROM property WHERE propertyID = ?');
+		$stmt->execute(array($propertyID));
+		$result = $stmt->fetch();
+		$pricePerDay = $result['price'];
+
+		return $pricePerDay * $days;
+	}
+
+	function createRent($propertyID, $touristID, $startDate, $endDate, $maxCancelDate, $price) {
+		global $conn;
+
+		$stmt = $conn->prepare('SELECT COUNT(rentID) as count FROM rent');
+		$stmt->execute();
+		$result = $stmt->fetch();
+		$newRendID = $result['count'] +1; 
+
+		$stmt = $conn->prepare('INSERT INTO rent VALUES (?, ?, ?, ?, ?, ?, ?)');
+		$stmt->execute(array($newRendID, $propertyID, $touristID, $startDate, $endDate, $maxCancelDate, $price));
+		return $newRendID;
+	}
+
 	function displayUserSProperty($property) {
 		echo '<h2> <a href="../pages/viewProperty.php?propertyID=' . $property['propertyID'] . '">' . $property['address'] . '</a> </h2>';
 		echo '<a id="editRents" href="../pages/editProperty.php?propertyID=' . $property['propertyID'] . '"> Edit </a>';
@@ -25,16 +77,15 @@
 
 	function displayPropertySRent($rent) {
 		echo '<div class="rent">';
-		echo '<div class="tourist">';
-		echo 	'<p><a href="../pages/user.php?userID=' . $rent['touristID']. '"> View Tourist </a></p> ';
-		echo '</div>';
-		echo 	'<p>Start date: ' . $rent['startDate'] . '</p>';
-		echo 	'<p>End date: ' . $rent['endDate'] . '</p>';
-		echo 	'<p>Last date to cancel: ' . $rent['cancelLimitDay'] . '</p>';
-    	echo 	'<p>Price: ' .  $rent['price'] . '</p>';
+		echo '	<div class="tourist">';
+		echo '		<p><a href="../pages/user.php?userID=' . $rent['touristID']. '"> View Tourist </a></p> ';
+		echo '	</div>';
+		echo '	<p>Start date: ' . $rent['startDate'] . '</p>';
+		echo '	<p>End date: ' . $rent['endDate'] . '</p>';
+		echo '	<p>Last date to cancel: ' . $rent['cancelLimitDay'] . '</p>';
+    	echo '	<p>Price: ' .  $rent['price'] . '</p>';
 		echo '</div>';
 	}
-
 
 	function getAllTouristRentsFromUser($username) {
 		global $conn;
